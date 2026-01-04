1289b072073deee8dd5ba7b2b3403699997b0fd2
fernandoromero05 <165194986+fernandoromero05@users.noreply.github.com>
2026-01-04 20:51:41 +0100

    Delete eiffel-bike directory

--- eiffel-bike/src/main/java/com/uge/ws/shop/ExchangeRateService.java
+++
@@ -1,6 +0,0 @@
-// src/main/java/com/uge/ws/shop/ExchangeRateService.java
-package com.uge.ws.shop;
-
-public interface ExchangeRateService {
-    double getRate(String fromCurrency, String toCurrency);
-}
