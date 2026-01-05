4b0b376f907186de37c896da3b0f85bb04354947
fernandoromero05 <165194986+fernandoromero05@users.noreply.github.com>
2026-01-05 18:01:59 +0100

    Delete eiffel-bike directory
    Faut l'effacer pour ramener la version finale

--- eiffel-bike/src/main/java/com/uge/ws/corp/RentalStatus.java
+++
@@ -1,6 +0,0 @@
-// src/main/java/com/uge/ws/corp/RentalStatus.java
-package com.uge.ws.corp;
-
-public enum RentalStatus {
-    ACTIVE, COMPLETED
-}
--- eiffel-bike/src/main/java/com/uge/ws/shop/ExchangeRateService.java
+++
@@ -1,5 +0,0 @@
-package com.uge.ws.shop;
-
-public interface ExchangeRateService {
-    double convert(String fromCurrency, String toCurrency, double amount);
-}
